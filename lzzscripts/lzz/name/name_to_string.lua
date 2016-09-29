1778b95fb1d9a0a72a2c159cd9e5c395014f6e97
mjspncr <mjspncr@gmail.com>
2016-09-28 22:49:56 -0400

    support brace initilizer on namespace obj, avoid >> in template name

--- lzzscripts/lzz/name/name_to_string.lua
+++ lzzscripts/lzz/name/name_to_string.lua
@@ -10,31 +10,37 @@ local function nameToString(name)
 end
 
 -- on qualified name
-function NameToString:onQualifiedName (name)
-   local s = '::' .. name.name:accept (self)
+function NameToString:onQualifiedName(name)
+   local s = '::' .. name.name:accept(self)
    if name.nested_name then
-      return name.nested_name:accept (self) .. s
+      return name.nested_name:accept(self) .. s
    end
    return s
 end
 
 -- on template name
-function NameToString:onTemplateName (name)
-   return string.format ('%s <%s>', name.base_name:accept (self), name.args)
+function NameToString:onTemplateName(name)
+   -- avoid >>
+   local args = name.args
+   if string.sub(args, -1) == '>' then
+      return string.format('%s <%s >', name.base_name:accept(self), name.args)
+   else
+      return string.format('%s <%s>',  name.base_name:accept(self), name.args)
+   end
 end
 
 -- on simple name
-function NameToString:onSimpleName (name)
+function NameToString:onSimpleName(name)
    return name.ident
 end
 
 -- on dtor name
-function NameToString:onDtorName (name)
+function NameToString:onDtorName(name)
    return '~ ' .. name.ident
 end
 
 -- on oper name
-function NameToString:onOperName (name)
+function NameToString:onOperName(name)
    return 'operator ' .. name.oper
 end
 
